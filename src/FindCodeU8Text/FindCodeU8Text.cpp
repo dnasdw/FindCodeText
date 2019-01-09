7dfc77a427d86ff5a4179d996600acd96655eaac
Sun Daowen <sundaowen@gmail.com>
2019-01-09 15:30:10 +0800

    update FindCodeU16Text and FindCodeU8Text

--- src/FindCodeU8Text/FindCodeU8Text.cpp
+++ src/FindCodeU8Text/FindCodeU8Text.cpp
@@ -55,7 +55,7 @@ int UMain(int argc, UChar* argv[])
 		return 1;
 	}
 	n32 nMethod = SToN32(argv[3]);
-	if (nMethod != 0 && nMethod != 1)
+	if (nMethod != 0 && nMethod != 1 && nMethod != 2)
 	{
 		return 1;
 	}
@@ -121,7 +121,49 @@ int UMain(int argc, UChar* argv[])
 					bool bASCII = count_if(sTxt.begin(), sTxt.end(), isASCII) == sTxt.size();
 					if (bIncludeASCIIOnly || !bASCII)
 					{
-						mOffsetAddress.insert(make_pair(i, uRamOffset));
+						mOffsetAddress.insert(make_pair(i, uRamOffset - 0x100000));
+						mOffsetSize.insert(make_pair(i, sTxtU8.size()));
+						mOffsetText.insert(make_pair(i, sTxt));
+					}
+				}
+			}
+		}
+	}
+	else if (nMethod == 2)
+	{
+		for (u32 i = 0; i < uCodeSize / 4 * 4; i += 4)
+		{
+			u32 uIns = *reinterpret_cast<u32*>(pCode + i);
+			// ADR
+			if ((uIns & 0xFFF0000) != 0x28F0000)
+			{
+				continue;
+			}
+			u32 uImm12 = uIns & 0xFFF;
+			u32 uUnrotatedValue = uImm12 & 0xFF;
+			u32 uRoRBits = (uImm12 >> 8 & 0xF) * 2;
+			// PC + 8
+			u32 uRamOffset = i + 0x100000 + 8;
+			uRamOffset += (uUnrotatedValue >> uRoRBits) | (uUnrotatedValue << (32 - uRoRBits));
+			if (uRamOffset % 2 == 0 && uRamOffset >= 0x100000 && uRamOffset < 0x100000 + uCodeSize)
+			{
+				string sTxtU8 = reinterpret_cast<char*>(pCode + uRamOffset - 0x100000);
+				wstring sTxt;
+				try
+				{
+					sTxt = U8ToW(sTxtU8);
+				}
+				catch (...)
+				{
+					continue;
+				}
+				bool bEmpty = sTxt.empty();
+				if (bIncludeEmpty || !bEmpty)
+				{
+					bool bASCII = count_if(sTxt.begin(), sTxt.end(), isASCII) == sTxt.size();
+					if (bIncludeASCIIOnly || !bASCII)
+					{
+						mOffsetAddress.insert(make_pair(i, uRamOffset - 0x100000));
 						mOffsetSize.insert(make_pair(i, sTxtU8.size()));
 						mOffsetText.insert(make_pair(i, sTxt));
 					}
